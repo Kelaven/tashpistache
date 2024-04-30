9c936712f7f4a590758fa2de4a2871e00c038765
Kelaven <kevin.lavenant@live.fr>
2024-04-30 16:45:07 +0200

    Fixtures tâches créées

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Task;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -16,6 +17,7 @@ class AppFixtures extends Fixture
         $this->userPasswordHasher = $userPasswordHasher;
     }
 
+
     public function load(ObjectManager $manager): void
     {
         // Création d'un user "normal" : user@email.fr password: password
@@ -27,8 +29,8 @@ class AppFixtures extends Fixture
             ->setPassword($this->userPasswordHasher->hashPassword($user, 'password'))
             ->setCreatedAt(new \DateTimeImmutable());
 
-
         $manager->persist($user);
+        $this->addReference('user_1', $user); // pour récupérer la références pour les fixtures des tâches et les rattacher à cet utilisateur
 
         // Création d'un user admin : admin@email.fr password: password
         $userAdmin = new User();
@@ -40,6 +42,34 @@ class AppFixtures extends Fixture
             ->setCreatedAt(new \DateTimeImmutable());
 
         $manager->persist($userAdmin);
+
+
+        // Flush pour s'assurer que les utilisateurs sont enregistrés et disposent d'un ID
+        $manager->flush();
+
+
+
+
+
+        // Création des tâches
+        for ($count = 0; $count < 10; $count++) { // cette boucle est créée pour créer 10 tâches
+            $task = new Task();
+
+            $task->setTitle('Titre' . $count)
+            ->setDescription('Description' . $count)
+            ->setStart(new \DateTime('2024-06-01'))
+            ->setEnd(new \DateTime('2024-06-30'))
+            ->setCreatedAt(new \DateTimeImmutable());
+            $user = $this->getReference('user_1'); // récupérer l'id 1 grâce à la méthode getReference des utilisateurs (grâce à l'ObjectManager) et le stocker dans $user
+            $task->setUser($user); // rattacher les tâches à l'utilisateur 1
+
+            $manager->persist($task);
+        }
+
+
+
+
+        // Flush pour les tâches
         $manager->flush();
     }
 }
